396e8730da48aff7cb0f6152435ae746bc3841df
H X <hpandey110301@gmail.com>
2019-09-01 12:27:54 +0530

    Update WQUPC.java

--- Union-Find/WQUPC.java
+++ Union-Find/WQUPC.java
@@ -1,17 +1,17 @@
 // Weighted quick union union-find with path compression
 
-public class WQUPC{
+public class WQUPC {
     private final int[] id;
     private final int[] sz;
 
     // Initialization -- O(n)
     public WQUPC(int n) {
-        if(n <= 0) {
+        if (n <= 0) {
             throw new IllegalArgumentException("Trying to initialize UF with non-positive size");
         }
         id = new int[n];
         sz = new int[n];
-        for (int i = 0;i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             id[i] = i;
             sz[i] = 1;
         }
@@ -19,12 +19,12 @@ public class WQUPC{
 
     // Root-finding and path compression -- O(lg n)
     private int root(int p) {
-        if(p <= 0 || p >= id.length){
+        if (p <= 0 || p >= id.length) {
             throw new IllegalArgumentException("p does not exist in the data structure");
         }
         int r = p;
         // Find root
-        while (r != id[r]){
+        while (r != id[r]) {
             r = id[r];
         }
         // Compress path
@@ -38,14 +38,14 @@ public class WQUPC{
 
     // Union operation -- O(lg n)
     public void union(int p, int q) {
-        if(p <= 0 || p >= id.length || q <= 0 || q >= id.length){
+        if (p <= 0 || p >= id.length || q <= 0 || q >= id.length) {
             throw new IllegalArgumentException("Either p or q does not exist in the data structure");
         }
         int proot = root(p);
         int qroot = root(q);
-        if(proot != qroot) {
+        if (proot != qroot) {
             // If p belongs to bigger tree then link q to p
-            if(sz[proot] > sz[qroot]){
+            if (sz[proot] > sz[qroot]) {
                 id[qroot] = proot;
                 sz[proot] += sz[qroot];
             } else {
@@ -56,15 +56,15 @@ public class WQUPC{
     }
 
     // Find operation -- O(lg n)
-    public boolean connected(int p, int q){
-        if(p <= 0 || p >= id.length || q <= 0 || q >= id.length){
+    public boolean connected(int p, int q) {
+        if (p <= 0 || p >= id.length || q <= 0 || q >= id.length) {
             throw new IllegalArgumentException("Either p or q does not exist in the data structure");
         }
         return (root(p) == root(q));
     }
 
     // To find the size of the DS
-    public int count(){
+    public int count() {
         return id.length;
     }
 
